8bb7832dd4e90dced91388229819f3c81f232835
鈴木潤一 <junichisuzuki@suzukijunichinoMacBook-puro.local>
2019-06-25 15:46:07 +0900

    [Update]コメント機能、いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'home/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
+  resources :books, only: [:welcome, :create, :index, :show, :edit, :destroy] do
+  	resource :favorites, only: [:create, :destroy]
+  	resource :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
   root to: 'books#welcome'
 end
